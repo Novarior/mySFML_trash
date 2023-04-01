a9ab7559b3ecd750b50a9679c6274f8ba3e1bba5
Your Name <you@example.com>
2023-04-02 01:08:25 +0300

    yep

--- build/CMakeFiles/CMakeDirectoryInformation.cmake
+++ build/CMakeFiles/CMakeDirectoryInformation.cmake
@@ -3,7 +3,7 @@
 
 # Relative path conversion top directories.
 set(CMAKE_RELATIVE_PATH_TOP_SOURCE "/Users/dmitrijsusenko/Desktop/try")
-set(CMAKE_RELATIVE_PATH_TOP_BINARY "/Users/dmitrijsusenko/Desktop/try/Build")
+set(CMAKE_RELATIVE_PATH_TOP_BINARY "/Users/dmitrijsusenko/Desktop/try/build")
 
 # Force unix paths in dependencies.
 set(CMAKE_FORCE_UNIX_PATHS 1)

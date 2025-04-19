f6588ec9f20b72f94f400a74b7fe49aa6f67f667
Novarior <Xle6yIIIeK1337@gmail.com>
2025-04-19 16:21:00 +0300

    test: check actions trigger

--- source/cmakever.h
+++ source/cmakever.h
@@ -1,10 +1,10 @@
 #ifndef PROJ_VER
 #define PROJ_VER
 
-const char CMAKE_PROJECT_VERSION[] = "0.0.11";
+const char CMAKE_PROJECT_VERSION[] = "0.0.10";
 const char CMAKE_PROJECT_VERSION_MAJOR[] = "0";
 const char CMAKE_PROJECT_VERSION_MINOR[] = "0";
-const char CMAKE_PROJECT_VERSION_PATCH[] = "11";
+const char CMAKE_PROJECT_VERSION_PATCH[] = "10";
 
 const char OPERATION_SYSTEM[] = "";
 
